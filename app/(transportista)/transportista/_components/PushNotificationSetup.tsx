2c4012ef19afaea4a91eaddbb6bdf65c7852e55b
nicohervith <n_hervith@hotmail.com>
2026-05-04 18:45:48 -0300

    fix notifications

--- app/(transportista)/transportista/_components/PushNotificationSetup.tsx
+++ app/(transportista)/transportista/_components/PushNotificationSetup.tsx
@@ -4,11 +4,13 @@ import { useEffect } from "react";
 
 const VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!;
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
   const rawData = atob(base64);
-  return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+  return new Uint8Array(
+    [...rawData].map((c) => c.charCodeAt(0)),
+  ) as Uint8Array<ArrayBuffer>;
 }
 
 export default function PushNotificationSetup() {
