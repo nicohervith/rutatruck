6efec5c092ce864d9597166c66c6959bb5347fbc
nicohervith <n_hervith@hotmail.com>
2026-06-12 15:31:53 -0300

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "prisma/config";
 import * as dotenv from "dotenv";
 
+//dotenv.config({ path: ".env.local", override: true });
 dotenv.config();
 
 export default defineConfig({
